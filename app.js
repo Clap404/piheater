b9065365ee5c09cb3d4614a131eca167bb0be776
Anthony Ruhier <anthony.ruhier@gmail.com>
2015-04-12 22:44:42 +0200

    Get real values for sensors and heater
    Remove the api router for code sharing

--- app.js
+++ app.js
@@ -31,10 +31,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var routes = require('./routes/index')(app);
-var api = require('./routes/api')(app);
 
 app.use('/', routes);
-app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,74 @@ module.exports = function(app){
     var SensorModel = sensormodels.Sensor;
     var HeaterModel = sensormodels.Heater;
 
+    var get_sensor_dict = function(callback) {
+        var STATIC_SENSORS = ["A200", "A240"];
+
+        models.gpio.sensor.getSensors(STATIC_SENSORS).
+            then(function(values){
+                var sensor_dict = {};
+
+                for(var i = 0; i < values.length; i++)
+                {
+                    sensor_id = STATIC_SENSORS[i];
+                    sensor_dict[sensor_id] = values[i];
+                }
+
+                callback(sensor_dict);
+            });
+    };
+
+    var get_heater_dict = function(callback){
+        var STATIC_HEATERS = {
+            pouet1: {alias: "pouet1", pins: [11, 12]},
+            pouet2: {alias: "pouet2", pins: [13, 15]},
+        };
+
+        var pins_lst = [];
+        for(var heater in STATIC_HEATERS)
+            pins_lst.push(STATIC_HEATERS[heater].pins);
+        var heaters = Object.keys(STATIC_HEATERS);
+
+        models.gpio.heater.getHeaters.apply(this, pins_lst).
+            then(function(modes){
+                var heater_lst = [];
+
+                for(var i = 0; i < modes.length; i++)
+                {
+                    heater_db = STATIC_HEATERS[heaters[i]];
+                    heater = {"alias": heater_db.alias, "mode": modes[i]};
+                    heater_lst.push(heater);
+                }
+
+                callback(heater_lst);
+            });
+
+    }
+
+    router.get("/api/sensor", function(req, res, next){
+        get_sensor_dict(function(sensor_dict){
+            res.json(sensor_dict);
+        });
+    });
+
+    router.get("/api/heater", function(req, res, next){
+        get_heater_dict(function(heater_lst){
+            res.json(heater_lst);
+        });
+    });
+
     /* GET home page. */
     router.get("/", function(req, res, next) {
-        res.render( "index", {
-            title: "Express",
-            sensor: {A200: 23, A240: 23.5},
-            heater: {"a010101":{"alias":"pouet","mode":"off"}},
+        get_sensor_dict(function(sensor_dict){
+            temparg = {
+                title: "Express",
+                sensor: sensor_dict,
+            };
+
+            get_heater_dict(function(heater_lst){
+                temparg["heater"] = heater_lst;
+                res.render("index", temparg);
+            })
         });
     });
 
